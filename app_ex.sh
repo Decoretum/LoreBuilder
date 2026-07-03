1e128e2c1d57f7d49ce04e4d84e5a56ce72b1a46
Gael Estrera <gaelthegoodboy@gmail.com>
2026-07-03 11:29:32 +0800

    Add Bash Scripyt for Mac users

---
+++ app_ex.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "\n\n"
+echo "Thank you so much for trying out this application. This application is
+for game enthusiasts and gamers in general wanting to flesh out the lore of
+their gaming character."
+echo "If you have any suggestions or comments, feel free to reach out on my
+social media or email"
+
+echo "Starting Frontend Application"
+
+cd lorebuilderfrontend
+npm run dev
+
